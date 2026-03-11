0d9c16fbd1e48917f6aeb0283dda8e5eafadb7da
vaibhav0809git <davhalevaibhav28@gmail.com>
2026-03-12 03:43:01 +0530

    fixed port

--- server/server.js
+++ server/server.js
@@ -342,6 +342,8 @@ io.emit("receiveMessage", msg)
 
 })
 
-server.listen(3000,()=>{
-console.log("Server running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
